fb7cff684538e190de409d712da3e44ba2a4d0ff
Matěj Foukal <matfou01@gmail.com>
2024-10-15 23:12:17 +0200

    feat: initial commit

---
+++ src/main/java/com/example/parsehub/ParseHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.parsehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParseHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/parsehub/ParseHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parsehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParseHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
