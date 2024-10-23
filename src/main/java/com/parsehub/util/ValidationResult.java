5b11a44aae598410592561505a44720d5908d7cb
Matěj Foukal <matfou01@gmail.com>
2024-10-24 01:01:26 +0200

    docs: add docstrings to ValidationResult

--- src/main/java/com/parsehub/util/ValidationResult.java
+++ src/main/java/com/parsehub/util/ValidationResult.java
@@ -3,22 +3,46 @@ package com.parsehub.util;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Utility class to represent the result of a validation operation.
+ */
 public class ValidationResult {
+    
     private boolean valid;
     private List<String> errorMessages = new ArrayList<>();
 
+    /**
+     * Returns the validation status.
+     *
+     * @return true if the validation was successful; false otherwise.
+     */
     public boolean isValid() {
         return valid;
     }
 
+    /**
+     * Sets the validation status.
+     *
+     * @param valid the validation status to be set. true indicates a successful validation.
+     */
     public void setValid(boolean valid) {
         this.valid = valid;
     }
 
+    /**
+     * Returns the list of error messages associated with the validation result.
+     *
+     * @return a list of error messages.
+     */
     public List<String> getErrorMessages() {
         return errorMessages;
     }
 
+    /**
+     * Adds a new error message to the list of error messages.
+     *
+     * @param errorMessage the error message to add to the list.
+     */
     public void addErrorMessage(String errorMessage) {
         this.errorMessages.add(errorMessage);
     }
